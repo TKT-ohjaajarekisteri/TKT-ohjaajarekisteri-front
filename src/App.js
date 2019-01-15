7dcaceb4d8b24de3d931d67f47402769c042739c
Virva <virva.svala@gmail.com>
2019-01-15 14:27:49 +0200

    test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React testu
+            Learn React testi
           </a>
         </header>
       </div>
