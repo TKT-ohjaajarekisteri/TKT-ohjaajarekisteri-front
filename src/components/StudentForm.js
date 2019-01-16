965ce0d4b4ed3b89e9db01327e1ceb2eddce4601
Virva <virva.svala@gmail.com>
2019-01-16 18:38:13 +0200

    StudentForm komponentin class:in  muutos funktioksi

--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -2,30 +2,31 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStudent} from './../reducers/studentReducer'
 
-class StudentForm extends React.Component {
+const StudentForm = (props) => {
 
-  handleSubmit = async (event) => {
+ const handleSubmit = async (event) => {
     event.preventDefault()
     const applicant = event.target.student.value
     const course = event.target.course.value
-    this.props.createStudent(applicant, course) 
-    //this.props.notify(`anecdote ${content} created`, 5)
+    props.createStudent(applicant, course) 
+    //props.notify(`anecdote ${content} created`, 5)
     event.target.student.value = ''
     event.target.course.value = ''
   }
-  render() {
+
     return (
       <div>
-        <h2>create new</h2>
-        <form onSubmit={this.handleSubmit}>
+        <h2>Create a new application</h2>
+        <form onSubmit={handleSubmit}>
+        Name:
           <div><input name='student'/></div>
+        Course:
           <div><input name='course'/></div>
           <button>create</button>
         </form>
       </div>
     )
   }
-}
 
 
 export default connect(
