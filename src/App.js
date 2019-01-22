f093f5c45f05db4b77350947d536c31f8c4b1918
Virva <virva.svala@gmail.com>
2019-01-22 19:44:31 +0200

    addin navigation links/routing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Course_studentList from './components/Course_studentList'
 import CourseList from './components/course/CourseList'
 import { initializeCourses } from './reducers/courseReducer'
 import { initializeStudents } from './reducers/studentReducer'
-//import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 
 const App=(props) => {
@@ -17,12 +17,33 @@ const App=(props) => {
   )
 
   return (
-    <div>
+    <div>  
       <h1>TKT Assistant Register</h1>
-      <CourseList />
-      <StudentList />
-      <Course_studentList />
-      <StudentForm />
+      <Router>
+        <div>
+          <div>
+            <Link to="/">form</Link> &nbsp;
+            <Link to="/courses">blogs</Link> &nbsp;
+            <Link to="/students">users</Link> &nbsp;
+            <Link to="/course_students">StudentsByCourse</Link> &nbsp;
+          </div>
+
+
+          <Route exact path="/" render={() => <StudentForm />} />
+          <Route path="/courses" render={() => <StudentList />} />
+          <Route path="/students" render={() => <CourseList />} />
+          <Route path="course_students" render={() => <Course_studentList />} />
+        </div>
+      </Router>
+
+
+      <div>
+        {/*  <h1>TKT Assistant Register</h1>
+       <CourseList />
+        <StudentList />
+        <Course_studentList />
+        <StudentForm /> */}
+      </div>
     </div>
   )
 }
