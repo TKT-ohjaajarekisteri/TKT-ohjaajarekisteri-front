e2bbe262f1a88d2b9e68ca3621044d77948e09d0
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2019-01-31 08:06:26 +0200

    app testausta

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,15 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
+
+let savedItems = {}
+
+const localStorageMock = {
+  setItem: (key, item) => {
+    savedItems[key] = item
+  },
+  getItem: (key) => savedItems[key],
+  clear: savedItems = {}
+}
+
+window.localStorage = localStorageMock
