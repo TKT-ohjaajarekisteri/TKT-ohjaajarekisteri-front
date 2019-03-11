b0a8a1d202e919e38ad9d88f254826a3cc10f4fa
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-03-11 18:24:07 +0200

    Added filtering based on study programme - Clicking button filters list - Clicking again resets filter, shows all

--- src/components/admin/CourseList.js
+++ src/components/admin/CourseList.js
@@ -2,45 +2,69 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import Course from './Course'
 import { initializeCourses } from '../../reducers/actionCreators/courseActions'
+import { initializeFilter, setProgramme } from '../../reducers/actionCreators/filterActions'
 
-
-export const CourseList = (props) => {
+export const CourseList = ({
+  initializeCourses,
+  initializeFilter,
+  setProgramme,
+  filter, courses
+}) => {
 
   useEffect(() => {
-    props.initializeCourses()
+    initializeCourses()
+    initializeFilter()
   },
-  []
+    []
   )
 
+  const handleProgrammeChange = (event) => {
+    event.preventDefault()
+    setProgramme(event.target.name)
+  }
+
   return (
-    <div className="courseList">
+    <div>
       <h2>Courses</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Code</th>
-            <th>Name</th>
-            <th>Year</th>
-            <th>Period</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.courses.map(course =>
-            <Course course={course} key={course.course_id} />
-          )}
-        </tbody>
-      </table>
+      <div>
+        <button type='submit' name='TKT' onClick={handleProgrammeChange}>CS-Bachelor</button>
+        <button type='submit' name='CSM' onClick={handleProgrammeChange}>CS-Master</button>
+        <button type='submit' name='DATA' onClick={handleProgrammeChange}>Data Science</button>
+      </div>
+
+      <div className="courseList">
+        <table>
+          <thead>
+            <tr>
+              <th>Code</th>
+              <th>Name</th>
+              <th>Year</th>
+              <th>Period</th>
+            </tr>
+          </thead>
+          <tbody>
+            {courses
+              .filter(course => course.learningopportunity_id.includes(filter.studyProgramme))
+              .map(course =>
+                <Course course={course} key={course.course_id} />
+              )}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
 
 const mapStateToProps = (state) => {
   return {
-    courses: state.courses
+    courses: state.courses,
+    filter: {
+      studyProgramme: state.filter.studyProgramme
+    }
   }
 }
 
 export default connect(
   mapStateToProps,
-  { initializeCourses }
+  { initializeCourses, initializeFilter, setProgramme }
 )(CourseList)
