fdcfcd493c99be903ddc6f732e1842a54143a3b8
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-04-15 11:38:58 +0300

    Student knows if he/she is accepted - Accepted badge - Delete-button is hidden to conserve history information

--- src/components/student/CourseWithDel.js
+++ src/components/student/CourseWithDel.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import DeleteButton from '../common/DeleteButton'
+import { Badge } from 'react-bootstrap'
 
 const CourseWithDel = ({ course, onClick }) => {
 
   return (
 
     <tr>
-      <td className = "learningopportunity_id">{course.learningopportunity_id}</td>
-      <td className = "name"> {course.course_name}</td>
-      <td className = "year">{course.year}</td>
-      <td className = "period">{course.period}</td>
-      <td className = "delete">
-        <DeleteButton
-          id={course.course_id}
-          onClick={ onClick }
-        />
+      <td className="learningopportunity_id">{course.learningopportunity_id}</td>
+      <td className="name"> {course.course_name}</td>
+      <td className="year">{course.year}</td>
+      <td className="period">{course.period}</td>
+
+      <td className="delete centerColumn">
+        {
+          course.Application.accepted ?
+            <Badge variant="success">Accepted</Badge>
+            :
+            <DeleteButton
+              id={course.course_id}
+              onClick={onClick}
+            />
+        }
       </td>
 
     </tr>
