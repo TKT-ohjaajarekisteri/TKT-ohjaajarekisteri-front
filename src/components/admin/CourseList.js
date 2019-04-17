a3e7735899cddd5910dd841546a0c8e62ce1b6f0
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-04-17 15:24:37 +0300

    Modified styles

--- src/components/admin/CourseList.js
+++ src/components/admin/CourseList.js
@@ -22,7 +22,7 @@ export const CourseList = ({
     initializeCourses()
     initializeFilter()
   },
-  []
+    []
   )
 
   const handleProgrammeChange = (event) => {
@@ -110,10 +110,10 @@ export const CourseList = ({
           <tr>
             <th>Code</th>
             <th>Name</th>
-            <th className='centerColumn' >Year</th>
-            <th className='centerColumn' >Period</th>
-            <th className='centerColumn' >Accepted/Applicants</th>
-            <th>Hidden</th>
+            <th className='centerColumn'>Year</th>
+            <th className='centerColumn'>Period</th>
+            <th className='centerColumn wrapSmallCell' >Accepted/ Applicants</th>
+            <th className='centerColumn'>Hidden</th>
           </tr>
         </thead>
         <tbody>
